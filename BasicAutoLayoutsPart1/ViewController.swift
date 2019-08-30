28c6d9644fc4b87cdade6de31d3ec3d4ad0fff35
BALA SHIVA SHANKAR TELUGU <balashivashankar11@Achiever-15.local>
2019-08-30 11:17:47 +0530

    first commit

---
+++ BasicAutoLayoutsPart1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicAutoLayoutsPart1
+//
+//  Created by BALA SHIVA SHANKAR TELUGU on 8/29/19.
+//  Copyright © 2019 BALA SHIVA SHANKAR TELUGU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
